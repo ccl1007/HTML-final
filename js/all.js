21dd218fe0d758d93b358917ecbda27928ed3030
ccl1007 <luku612150@gmail.com>
2021-09-28 21:06:50 +0800

    HTML-final_v3

--- js/all.js
+++ js/all.js
@@ -1,44 +1,45 @@
 $(document).ready(function () {
-    $(".navbarItem>a").click(function (e) {
-        e.preventDefault();
-        $(this).addClass('active').siblings(".dropdown-open").slideToggle(500);
-        $(this).parent().siblings().find("ul").slideUp(500).parent().siblings().find("a").removeClass('active');
-      });
+  $(".navbarItem>a").hover(function (e) {
+    e.preventDefault();
+    $(this).addClass('active').parent().siblings().find("a").removeClass('active');
+    $(this).siblings(".dropdown-open").slideDown(500);
+    $(this).parent().siblings().find(".dropdown-open").slideUp(500);
+  });
 
 
-      const swiper = new Swiper('.swiper', {
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true,
-        effect:'fade',
-        fadeEffect: { crossFade: true },
-        speed:1000,
-        autoplay: {
-         delay: 3000,
-       },
-      
-        // If we need pagination
-        pagination: {
-          el: '.swiper-pagination',
-        },
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      
-       
-      });
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    effect: 'fade',
+    fadeEffect: { crossFade: true },
+    speed: 1000,
+    autoplay: {
+      delay: 3000,
+    },
 
-      lightbox.option({
-        //是否重複撥放
-        //wrapAround: true,
-        fadeDuration: 700,
-        resizeDuration: 700
-      });
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
 
-      $("#gotop").click(function (e) { 
-        e.preventDefault();
-        $("html,body").animate({scrollTop: 0}, 1000);
-      });
+
+  });
+
+  lightbox.option({
+    //是否重複撥放
+    //wrapAround: true,
+    fadeDuration: 700,
+    resizeDuration: 700
+  });
+
+  $("#gotop").click(function (e) {
+    e.preventDefault();
+    $("html,body").animate({ scrollTop: 0 }, 1000);
+  });
 });
